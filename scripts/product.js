930be613ef338b0d26381d8caf41dbc62e68be4b
Lenin <sclenins@gmail.com>
2024-08-29 14:32:34 -0500

    Se modificó script productCard.js

--- scripts/product.js
+++ scripts/product.js
@@ -1,100 +1,25 @@
 class Product {
-  constructor(id, title, price, stock, images, onsale, supplier) {
+  constructor(
+    id,
+    linkToPage,
+    linkToImg,
+    title,
+    subTitle,
+    price,
+    stock,
+    discount,
+    supplier,
+    taxes
+  ) {
     this.id = id;
+    this.linkToPage = linkToPage;
+    this.linkToImg = linkToImg;
     this.title = title;
+    this.subTitle = subTitle;
     this.price = price;
     this.stock = stock;
-    this.images = images;
-    this.onsale = onsale;
+    this.discount = discount;
     this.supplier = supplier;
+    this.taxes = taxes;
   }
-
-  get getSupplier() {
-    return this.supplier;
-  }
-
-  set setSupplier(newName) {
-    this.supplier = newName;
-  }
-
-  sellUnits(units) {
-    if (this.stock >= units) {
-      this.stock = this.stock - units;
-    } else {
-      console.log("Error, stock no disponible.");
-    }
-  }
-}
-
-const prod1 = new Product(
-  "MAC-2024-1",
-  "Mac Mini Intel",
-  4200,
-  12,
-  [],
-  true,
-  "Lenin"
-);
-
-const prod2 = new Product(
-  "MAC-2024-2",
-  "Mac Mini Intel",
-  2500,
-  12,
-  [],
-  true,
-  "Lenin"
-);
-
-const prod3 = new Product(
-  "MAC-2024-3",
-  "Mac Mini Intel",
-  3500,
-  12,
-  [],
-  true,
-  "Lenin"
-);
-
-const prod4 = new Product(
-  "MAC-2024-4",
-  "Mac Mini Intel",
-  4000,
-  12,
-  [],
-  true,
-  "Lenin"
-);
-
-const products = [prod1, prod2, prod3, prod4];
-
-for(let i = 0; i < products.length; i++){
-  console.log(products[i]);
-}
-
-const prod0 = new Product(
-  "MAC-2024-0",
-  "Mac Mini Intel",
-  3500,
-  12,
-  [],
-  true,
-  "Lenin"
-);
-
-const prod5 = new Product(
-  "MAC-2024-5",
-  "Mac Mini Intel",
-  4000,
-  12,
-  [],
-  true,
-  "Lenin"
-);
-
-products.unshift(prod0);
-products.push(prod5);
-
-for(let i = 0; i < products.length; i++){
-  console.log(products[i]);
 }
--- scripts/productCard.js
+++ scripts/productCard.js
@@ -1,98 +1,67 @@
 const productSelector = document.getElementById("article-card");
 
-const products = [
-  { 
-    title: "Apple watch Series 8 GPS", 
-    description: "Smartwatch de última generación", 
-    price: "$400.00", 
-    discount: "10% Off", 
-    linkTo: "pages/detallesProducto.html", 
-    imgSrc: "/images/products/Apple-watch-Series-8-GPS.jpg" 
-  },
-  { 
-    title: "MacBook Air 13 Chip M2", 
-    description: "Laptop ultradelgada con chip M2", 
-    price: "$1,200.00", 
-    discount: "15% Off", 
-    linkTo: "pages/detallesProducto.html", 
-    imgSrc: "/images/products/MacBook-Air-13-Chip-M2.jpg" 
-  },
-  { 
-    title: "iPad Pro 12.9 6ta Generación con Chip M2", 
-    description: "Tablet avanzada con chip M2", 
-    price: "$1,000.00", 
-    discount: "20% Off", 
-    linkTo: "pages/detallesProducto.html", 
-    imgSrc: "/images/products/iPad-Pro-12.9-6ta-Generación-con-Chip-M2.jpg" 
-  },
-  { 
-    title: "iPhone 15 Pro Max", 
-    description: "Último modelo de iPhone", 
-    price: "$1,500.00", 
-    discount: "5% Off", 
-    linkTo: "pages/detallesProducto.html", 
-    imgSrc: "/images/products/iPhone-15-Pro-Max.jpg" 
-  },
-  { 
-    title: "AirPods Pro", 
-    description: "Auriculares inalámbricos", 
-    price: "$250.00", 
-    discount: "25% Off", 
-    linkTo: "pages/detallesProducto.html", 
-    imgSrc: "/images/products/AirPods-Pro.jpg" 
-  },
-];
-
-for (let product of products) {
-    const element = document.createElement("article");
-    element.className = "product-card";
-
-    const link = document.createElement("a");
-    link.href = product.linkTo;
-
-    const img = document.createElement("img");
-    img.className = "product-img";
-    img.src = product.imgSrc;
-    img.alt = product.title;
-
-    const infoDiv = document.createElement("div");
-    infoDiv.className = "product-info";
-
-    const titleSpan = document.createElement("span");
-    titleSpan.className = "product-title";
-    titleSpan.textContent = product.title;
-
-    const descriptionSpan = document.createElement("span");
-    descriptionSpan.className = "product-description";
-    descriptionSpan.textContent = product.description;
-
-    const priceBlockDiv = document.createElement("div");
-    priceBlockDiv.className = "product-price-block";
-
-    const priceSpan = document.createElement("span");
-    priceSpan.className = "price";
-    priceSpan.textContent = product.price;
-
-    const discountSpan = document.createElement("span");
-    discountSpan.className = "discount";
-    discountSpan.textContent = product.discount;
-
-    priceBlockDiv.appendChild(priceSpan);
-    priceBlockDiv.appendChild(discountSpan);
-
-    const taxPolicyDiv = document.createElement("div");
-    taxPolicyDiv.className = "product-tax-policy";
-    taxPolicyDiv.textContent = "Incluye impuestos";
-
-    infoDiv.appendChild(titleSpan);
-    infoDiv.appendChild(descriptionSpan);
-    infoDiv.appendChild(priceBlockDiv);
-    infoDiv.appendChild(taxPolicyDiv);
-
-    link.appendChild(img);
-    link.appendChild(infoDiv);
-
-    element.appendChild(link);
-
-    productSelector.appendChild(element);
+function createCard(product) {
+  const formattedPrice = formatPrice(product.price);
+  return `
+      <article class="product-card">
+          <a href="${product.linkToPage}"
+            ><img
+              class="product-img"
+              src="${product.linkToImg}"
+              alt="${product.title}"
+            />
+            <div class="product-info">
+              <span class="product-title">${product.title}</span
+              ><span class="product-description">${product.subTitle}</span>
+              <div class="product-price-block">
+                <span class="price">${formattedPrice}</span
+                ><span class="discount">${product.discount}% Off</span>
+              </div>
+              <div class="product-tax-policy">${
+                product.taxes ? "Incluye impuestos" : "No incluye impuestos"
+              }</div>
+            </div></a
+          >
+        </article>
+   `;
 }
+
+function formatPrice(price) {
+  return parseFloat(price).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+}
+
+async function loadExcelFile() {
+  try {
+    const response = await fetch("/data/products.xlsx");
+    const arrayBuffer = await response.arrayBuffer();
+    const workbook = XLSX.read(arrayBuffer, { type: "array" });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const json = XLSX.utils.sheet_to_json(sheet);
+
+    const products = json.map((row) => ({
+      id: row.id,
+      linkToPage: row.linkToPage,
+      linkToImg: row.linkToImg,
+      title: row.title,
+      subTitle: row.subTitle,
+      price: row.price,
+      discount: row.discount,
+      taxes: row.taxes,
+    }));
+
+    showProductCards(products);
+  } catch (error) {
+    console.error("Error loading or processing the Excel file", error);
+  }
+}
+
+function showProductCards(products) {
+  const productsTemplate = products.map(createCard).join("");
+  productSelector.innerHTML = productsTemplate;
+}
+
+loadExcelFile();
